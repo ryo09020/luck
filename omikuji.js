eced332505cb822751393b630334ac19a67b1f5b
ryo09020 <ryozinl924@gmail.com>
2022-12-14 17:17:24 +0900

    OMIKUJI

---
+++ omikuji.js
@@ -0,0 +1,22 @@
+let user;
+let usresult;
+user=prompt("お名前は？");
+document.getElementById("name").innerHTML=user;
+
+let rand=Math.floor(Math.random()*5);
+if(rand==0){
+    usresult="大吉";
+}
+if(rand==1){
+    usresult="中吉";
+}
+if(rand==2){
+    usresult="小吉";
+}
+if(rand==3){
+    usresult="吉";
+}
+if(rand==4){
+    usresult="凶";
+}
+document.getElementById("result").innerHTML=usresult;
